585438f17a4ebdaff9b9a15ecefeb7be81f79057
Ronald Mannak <ronaldmannak@me.com>
2026-03-01 22:21:11 -0800

    swift-lint

--- Libraries/MLXLMCommon/Extensions/JSONDecoder+JSON5.swift
+++ Libraries/MLXLMCommon/Extensions/JSONDecoder+JSON5.swift
@@ -2,8 +2,8 @@
 
 import Foundation
 
-public extension JSONDecoder {
-    static func json5() -> JSONDecoder {
+extension JSONDecoder {
+    public static func json5() -> JSONDecoder {
         let decoder = JSONDecoder()
         decoder.allowsJSON5 = true
         return decoder
